cbabcc01c06bb539fa0200bec254a5ddf65d3075
Franck Montaigne <franck.montaigne@amalto.com>
2017-10-20 16:48:04 +0200

    WIP on common components

---
+++ components/todo.js
@@ -0,0 +1,52 @@
+/*
+
+b2portal.compileWordings = compileWordings
+b2portal.addValToArrayNoDup = addValToArrayNoDup
+b2portal.removeValFromArrayNoDup = removeValFromArrayNoDup
+b2portal.filterCollection = filterCollection
+
+b2portal.ColorPicker = ColorPicker
+b2portal.DatePicker = DatePicker
+b2portal.TimePicker = TimePicker
+b2portal.ValidatedInput = ValidatedInput
+b2portal.FileInput = FileInput
+b2portal.PdfViewer = PdfViewer
+
+b2portal.FileImporter = FileImporter
+b2portal.LanguageWrapper = LanguageWrapper
+
+b2portal.RadioInput = RadioInput
+b2portal.CheckboxInput = CheckboxInput
+b2portal.CheckboxesInput = CheckboxesInput
+b2portal.SelectInput = SelectInput
+b2portal.TextInput = TextInput
+b2portal.ReadOnlyInput = ReadOnlyInput
+b2portal.SwitchInput = SwitchInput
+b2portal.TextareaInput = TextareaInput
+b2portal.DateInput = DateInput
+b2portal.TimeInput = TimeInput
+b2portal.CodeEditorInput = CodeEditorInput
+b2portal.CodeEditor = CodeEditor
+b2portal.CronInput = CronInput
+b2portal.TypeaheadInput = TypeaheadInput
+
+b2portal.validationUtils = validationUtils
+
+b2portal.TogglePanel = TogglePanel
+b2portal.Spinner = Spinner
+b2portal.DataGrid = DataGrid
+b2portal.DataLine = DataLine
+b2portal.Tabs = Tabs
+b2portal.Tab = Tab
+b2portal.ButtonsBar = ButtonsBar
+b2portal.Tree = Tree
+
+b2portal.DynamicComponent = DynamicComponent
+
+b2portal.CustomFormDialog = CustomFormDialog
+
+b2portal.Restricted = Restricted
+
+b2portal.scopesHelpers = scopesHelpers
+
+*/
