cf6cadc5a0056bc6a089bf85708fe603ac585418
Christopher Vuong <christopher.vuong@amalto.com>
2017-12-11 18:05:14 +0100

    Use action for ActionButton

--- stories/index.js
+++ stories/index.js
@@ -19,26 +19,26 @@ storiesOf('Components', module)
                     <ActionButton btnClass='btn btn-trans btn-info mgt-10 mgr-5'
                         iconClass='fa fa-info'
                         tooltipText='Info button'
-                        onClick={action('transparent info button')}
+                        clickAction={action('transparent info button')}
                     />
                     <ActionButton btnClass='btn btn-trans btn-danger mgt-10 mgr-5'
                         iconClass='fa fa-trash'
-                        onClick={action('transparent danger button')}
+                        clickAction={action('transparent danger button')}
                         tooltipText='Danger button'
                     />
                     <ActionButton btnClass='btn btn-trans btn-warning mgt-10 mgr-5'
                         iconClass='fa fa-exclamation'
-                        onClick={action('transparent warning button')}
+                        clickAction={action('transparent warning button')}
                         tooltipText='Warning button'
                     />
                     <ActionButton btnClass='btn btn-trans btn-primary mgt-10 mgr-5'
                         iconClass='fa fa-check'
-                        onClick={action('transparent primary button')}
+                        clickAction={action('transparent primary button')}
                         tooltipText='Primary button'
                     />
                     <ActionButton btnClass='btn btn-trans btn-font mgt-10 mgr-5'
                         iconClass='fa fa-shield'
-                        onClick={action('transparent font button')}
+                        clickAction={action('transparent font button')}
                         tooltipText='Default button'
                     />
                 </div>
@@ -47,27 +47,27 @@ storiesOf('Components', module)
                     <h4 className='mgt-10'>Colored background</h4>
                     <ActionButton btnClass='btn btn-info mgt-10 mgr-5'
                         iconClass='fa fa-info'
-                        onClick={action('colored info button')}
+                        clickAction={action('colored info button')}
                         tooltipText='Info button'
                     />
                     <ActionButton btnClass='btn btn-danger mgt-10 mgr-5'
                         iconClass='fa fa-trash'
-                        onClick={action('colored danger button')}
+                        clickAction={action('colored danger button')}
                         tooltipText='Danger button'
                     />
                     <ActionButton btnClass='btn btn-warning mgt-10 mgr-5'
                         iconClass='fa fa-exclamation'
-                        onClick={action('colored warning button')}
+                        clickAction={action('colored warning button')}
                         tooltipText='Warning button'
                     />
                     <ActionButton btnClass='btn btn-primary mgt-10 mgr-5'
                         iconClass='fa fa-check'
-                        onClick={action('colored primary button')}
+                        clickAction={action('colored primary button')}
                         tooltipText='Primary button'
                     />
                     <ActionButton btnClass='btn btn-font mgt-10 mgr-5'
                         iconClass='fa fa-shield'
-                        onClick={action('colored font button')}
+                        clickAction={action('colored font button')}
                         tooltipText='Default button'
                     />
                 </div>
@@ -90,7 +90,7 @@ storiesOf('Components', module)
 
         return (
             <div className='mgt-10'>
-                <KeyValueEditor id="treeComponentExemple" handleChange={e => action('handleChange')} keyValues={values} locale='en-US' />
+                <KeyValueEditor id="treeComponentExemple" handleChange={keyValues => action('handleChange')} keyValues={values} locale='en-US' />
             </div>
         )
     })
