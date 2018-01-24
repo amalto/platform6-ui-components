ec477782eaccb00ff5a855a3faf062dee008645d
Christopher Vuong <christopher.vuong@amalto.com>
2018-01-24 17:39:45 +0100

    Fix module name and install component from @amalto scope

--- components/paging-controls/typescript/index.tsx
+++ components/paging-controls/typescript/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
-import * as ReactDOM from 'reactdom'
+import * as ReactDOM from 'react-dom'
 
-import { compileWordings } from 'helpers'
+import { compileWordings } from '@amalto/helpers'
 
 //modules
-import { FormattedNumber } from 'reactintl'
+import { FormattedNumber } from 'react-intl'
 import * as classNames from 'classnames'
 
 const WORDINGS = {
