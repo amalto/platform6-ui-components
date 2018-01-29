56e85887442ecf3a37c274f68b1eae86185bcabf
Christopher Vuong <christopher.vuong@amalto.com>
2018-01-29 15:54:25 +0100

    action-button test done

--- components/action-button/test/test.js
+++ components/action-button/test/test.js
@@ -11,15 +11,47 @@ import {
 
 configure({ adapter: new Adapter() });
 
-test('should have .action-icon-button class name', t => {
+test.beforeEach('should component exist', t => {
     const wrapper = shallow(<ActionButton />);
 
-    t.true(wrapper.hasClass('action-icon-button'));
+    t.true(wrapper.exists());
 });
 
-test('children should have classname .fa .fa-info', t => {
-    const wrapper = shallow(<ActionButton iconClass='fa-info' />);
+test('should have class name .action-icon-button .disabled', t => {
+    const wrapper = shallow(<ActionButton disabled={true} btnClass='btn btn-trans btn-info' />);
+
+    t.true(wrapper.hasClass('action-icon-button'));
+    t.true(wrapper.hasClass('disabled'));
+    t.true(wrapper.hasClass('btn btn-trans btn-info'));
+});
+
+test('children should have classname .fa .fa-info .info-color', t => {
+    const wrapper = shallow(<ActionButton iconClass='fa-info' colorClass='info-color' />);
 
     t.is(wrapper.children('span').length, 1);
     t.true(wrapper.children('span').hasClass('fa fa-info'));
+    t.true(wrapper.children('span').hasClass('info-color'));
 });
+
+test('should display a tooltip', t => {
+    const wrapper = shallow(<ActionButton tooltipText={'Hello world'} />);
+
+    /**
+     * Check props data-original-title because it's the wrapper props associated to tooltipText
+     */
+    t.deepEqual(wrapper.prop('data-original-title'), 'Hello world');
+    t.notDeepEqual(wrapper.prop('data-original-title'), 'Goodbye world');
+});
+
+test('should trigger clickAction', t => {
+    let count = 0;
+    const clickAction = e => {
+        // Do something here
+        count += 1;
+    };
+    const wrapper = shallow(<ActionButton clickAction={clickAction} />);
+
+    wrapper.simulate('click');
+    t.is(count, 1);
+    t.not(count, 0);
+})
