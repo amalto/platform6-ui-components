90268b68b07443606c60e8c11989a8b97048bef4
Christopher Vuong <christopher.vuong@amalto.com>
2018-01-30 10:21:05 +0100

    Add test for switch

---
+++ components/switch/test/test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import test from 'ava';
+import Adapter from 'enzyme-adapter-react-15';
+import * as sinon from 'sinon';
+
+import Switch from '../build/index.js';
+import {
+    shallow,
+    configure
+} from 'enzyme';
+
+configure({ adapter: new Adapter() });
+
+test.beforeEach('component should exist', t => {
+    const wrapper = shallow(<Switch />);
+
+    t.true(wrapper.exists());
+});
+
+test('should work will all props', t => {
+    const changeHandler = (value, name) => {
+        console.warn('changeHandler method as been called');
+    };
+    const spy = sinon.spy(changeHandler);
+    const wrapper = shallow(
+        <Switch id='id-test-swipe-component'
+            value={true}
+            cssClass='padded'
+            alignLeft={false}
+            name='name-test-swipe-component'
+            changeHandler={spy}
+        />
+    );
+
+    t.true(wrapper.hasClass('padded'));
+    t.true(wrapper.children().hasClass('onoffswitch'));
+
+    // check input
+    t.is(wrapper.children().children().at(0).prop('id'), 'id-test-swipe-component');
+    t.is(wrapper.children().children().at(0).prop('checked'), true);
+    t.is(wrapper.children().children().at(0).prop('name'), 'name-test-swipe-component');
+
+    // triggering onchange event on input
+    wrapper.children().children().at(0).props().onChange({ target: { checked: true } });
+    t.true(spy.called);
+});
