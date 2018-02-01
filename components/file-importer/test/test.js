38ea22bb4a60161a4b70b2d806140923feef23c0
Christopher Vuong <christopher.vuong@amalto.com>
2018-02-01 11:53:19 +0100

    file-importer test implemented

--- components/file-importer/test/test.js
+++ components/file-importer/test/test.js
@@ -36,9 +36,36 @@ test('FileImporter props should be defined', t => {
             fileData={{ name: 'test_file', size: 100 }}
             cancelHandler={spyCancelHandler}
             submitHandler={spySubmitHandler}
+            hideControls={{
+                separator: true,
+                encoding: true,
+                overwrite: true
+            }}
             processing={true}
             spinnerSrc={'/images/spinner.gif'}
         />
     );
-    t.pass();
+
+    t.true(wrapper.hasClass('form-dialog'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(0).childAt(0).hasClass('panel-title has-spinner'));
+    t.is(wrapper.childAt(0).childAt(0).childAt(0).childAt(0).text(), 'Import data from a file');
+    t.true(wrapper.childAt(0).childAt(0).childAt(0).childAt(1).hasClass('spinner-container'));
+    t.is(wrapper.childAt(0).childAt(0).childAt(0).childAt(1).childAt(0).prop('src'), '/images/spinner.gif');
+
+    t.is(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).children().length, 6);
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(0).hasClass('col-xs-12 col-sm-6'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(1).hasClass('col-xs-12 col-sm-6 hidden'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(2).hasClass('col-xs-12 col-sm-6'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(3).hasClass('col-xs-12 col-sm-6 hidden'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(4).hasClass('col-xs-12 col-sm-6'));
+    t.true(wrapper.childAt(0).childAt(0).childAt(1).childAt(1).childAt(5).hasClass('col-xs-12 col-sm-6 hidden'));
+
+
+    t.false(spyCancelHandler.called);
+    wrapper.childAt(0).childAt(0).childAt(2).childAt(0).simulate('click');
+    t.true(spyCancelHandler.called);
+
+    t.false(spySubmitHandler.called);
+    wrapper.childAt(0).childAt(0).childAt(2).childAt(1).simulate('click');
+    t.true(spySubmitHandler.called);
 });
