d3aab56c1a1c541fbb6e445f577d0b965752746c
Christopher Vuong <christopher.vuong@amalto.com>
2018-04-16 09:15:20 +0200

    Typings export ColumnHeader, CellData and DataGridTemplates

--- components/typings/index.tsx
+++ components/typings/index.tsx
@@ -14,6 +14,7 @@ import NotificationModel from './models/NotificationModel'
 import { UserModel, getUserJson, UserSearch } from './models/UserModel'
 import { WebApi } from './models/WebApi'
 import DynamicComponent from './models/DynamicComponent'
+import { ColumnHeader, CellData, DataGridTemplates } from './models/DataGrid'
 
 export {
     Id,
@@ -30,3 +31,4 @@ export { NotificationModel }
 export { UserModel, getUserJson, UserSearch }
 export { WebApi }
 export { DynamicComponent }
+export { ColumnHeader, CellData, DataGridTemplates }
