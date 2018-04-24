60ed1a121b6773f70709567e52dcbed30b1a6a42
Christopher Vuong <christopher.vuong@amalto.com>
2018-04-24 17:39:43 +0200

    Update BaseListConfig interface

--- components/typings/models/BaseListConfig.ts
+++ components/typings/models/BaseListConfig.ts
@@ -1,9 +1,9 @@
-import { ServiceItemFacade } from '@amalto/service-helpers'
+import { ServiceItemFacade, ServiceItemFacades } from '@amalto/service-helpers'
 
 export interface BaseListConfig {
     viewTabRenderer: ( viewId: string, item: ServiceItemFacade, closeTab: () => void ) => JSX.Element;
-    editTabRenderer: ( editId: string, item: ServiceItemFacade, closeTab: () => void ) => JSX.Element;
-    addTabRenderer: ( addId: string, closeTab: () => void ) => JSX.Element;
+    editTabRenderer: ( editId: string, item: ServiceItemFacade, closeTab: () => void, items: ServiceItemFacades ) => JSX.Element;
+    addTabRenderer: ( addId: string, closeTab: () => void, items: ServiceItemFacades ) => JSX.Element;
     readPermission?: string;
     editPermission?: string;
     deletePermission?: string;
