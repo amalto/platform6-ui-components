2ca442219920ab9f533ffe54c7affeaf12ab86fb
Franck Montaigne <franck.montaigne@amalto.com>
2018-06-14 10:51:17 +0200

    Update on Dyn Component props interface

--- components/service-helpers/models/DynamicComponent.ts
+++ components/service-helpers/models/DynamicComponent.ts
@@ -15,25 +15,25 @@ export interface DynamicComponent extends ReduxProps {
 
     /** Interface used to perform all the api call to the server. */
     api: WebApi;
-    
+
     /** Allow you to handle your navigation. */
     appHistory: History;
 
     /** Actions controlling the DataGrid template such as sorting order or column width. */
     dataGridActions: any;
-    
+
     /** Component to be display by an eval. */
     componentScript: string;
-    
+
     /** Component data to be passed has props. */
     componentData?: any;
-    
+
     /** Refresh data to be passed to component. */
     refreshData?: () => void;
-    
+
     /** Context data saved into store to be restored on serveur. */
     contextData?: any;
-    
+
     /**
      * Store component data inside the store as contextData.
      * @param { any } contextData
@@ -45,7 +45,7 @@ export interface DynamicComponent extends ReduxProps {
      * @param { UserModel } payload
      */
     saveDataGridTemplate?: ( payload: UserModel ) => void;
-    
+
     /**
      * Display column header configuration popup.
      * @param { any } content
@@ -53,7 +53,7 @@ export interface DynamicComponent extends ReduxProps {
      * @param { number } [positionY]
      */
     displayContextMenu: ( content: any, positionX?: number, positionY?: number ) => void;
-    
+
     /**
      * Hide context menu.
      */
@@ -69,38 +69,40 @@ export interface DynamicComponent extends ReduxProps {
 
     /** Component height. */
     height?: number;
-    
+
     /** Component fullscreen status. */
     fullscreen?: boolean;
-    
+
     /** Manage component fullscreen status. */
     setFullscreen?: ( fullscreen: boolean ) => void;
-    
+
     /** Reload tooltip if not displayed correctly. */
     reloadTooltips?: () => void;
-    
+
     /**
      * Show current job status.
      * @param { string | number } jobId
-     * @param { ( job: RunningJob ) => void } callbackOnComplete
      */
-    showJobStatus?: ( jobId: string | number, callbackOnComplete?: ( job: RunningJob ) => void ) => void;
-    
+    showJobStatus?: ( jobId: string | number ) => void;
+
+    /** List of complete jobs IDs */
+    completeJobs?: number[];
+
     /** If any application publisher profile is accessible to user. */
     canSelectAppKey?: boolean;
 
     /** Current application publisher profile object selected. */
     selectedAppKey?: AppKey;
-    
+
     /** Current application publisher profile name selected. */
     selectedAppKeyName?: string;
-    
+
     /**
      * Application publisher profile is installed.
      * @param { string } appKeyName
      */
     hasAppKeyInstalled?: ( appKeyName: string ) => boolean;
-    
+
     /**
      * Handle display of bach operations in case some of the operation fails and other succeed.
      * @param { BatchOperationReport } report
@@ -117,16 +119,16 @@ export interface DynamicComponent extends ReduxProps {
      * @param { string[] } [itemsList]
      */
     showDialog: ( title: string, body: React.ReactElement<any> | string, confirmAction?: any, cancelAction?: any, confirmLevel?: string, itemsList?: string[] ) => void;
-    
+
     /** Hide the confirmation modal. */
     hideDialog: () => void;
-    
+
     /**
      * Display a custom notification. More details on [NotificationModel](#notificationmodel).
      * @param { NotificationModel } notificationOptions
      */
     displayNotification: ( notificationOptions: NotificationModel ) => void;
-    
+
     /**
      * Display an error notification for failed request.
      * @param { any } error
