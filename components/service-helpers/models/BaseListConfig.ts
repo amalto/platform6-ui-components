e19e9970081f50dbc7ecb4209fc23af4aac59c54
Franck Montaigne <franck.montaigne@amalto.com>
2018-06-14 14:03:16 +0200

    Added refreshItems method to base list config interface

--- components/service-helpers/models/BaseListConfig.ts
+++ components/service-helpers/models/BaseListConfig.ts
@@ -2,8 +2,8 @@ import { Id, ServiceItemFacade, ServiceItemFacades } from './ServiceHelpers'
 import ButtonsBar from '@amalto/buttons-bar'
 
 export interface BaseListConfig {
-    viewTabRenderer: ( viewId: string, item: ServiceItemFacade, closeTab: () => void ) => JSX.Element;
-    editTabRenderer: ( editId: string, item: ServiceItemFacade, closeTab: () => void, items: ServiceItemFacades, formId: string ) => JSX.Element;
+    viewTabRenderer: ( viewId: string, item: ServiceItemFacade, closeTab: () => void, refreshItems: () => void ) => JSX.Element;
+    editTabRenderer: ( editId: string, item: ServiceItemFacade, closeTab: () => void, items: ServiceItemFacades, formId: string, refreshItems: () => void ) => JSX.Element;
     addTabRenderer: ( addId: string, closeTab: () => void, items: ServiceItemFacades, formId: string, openEditTab?: ( tabId: string, id: Id ) => void ) => JSX.Element;
     readPermission?: string;
     editPermission?: string;
