0f3bab5a46d75b294f49c414e2916a18c521b1e0
Franck Montaigne <franck.montaigne@amalto.com>
2018-07-02 13:18:15 +0200

    Service Base List config change: Optional viewTabRenderer

--- components/service-helpers/models/BaseListConfig.ts
+++ components/service-helpers/models/BaseListConfig.ts
@@ -2,7 +2,7 @@ import { Id, ServiceItemFacade, ServiceItemFacades } from './ServiceHelpers'
 import ButtonsBar from '@amalto/buttons-bar'
 
 export interface BaseListConfig {
-    viewTabRenderer: ( viewId: string, item: ServiceItemFacade, closeTab: () => void, refreshItems: () => void ) => JSX.Element;
+    viewTabRenderer?: ( viewId: string, item: ServiceItemFacade, closeTab: () => void, refreshItems: () => void ) => JSX.Element;
     editTabRenderer: ( editId: string, item: ServiceItemFacade, closeTab: () => void, items: ServiceItemFacades, formId: string, refreshItems: () => void ) => JSX.Element;
     addTabRenderer: ( addId: string, closeTab: () => void, items: ServiceItemFacades, formId: string, openEditTab?: ( tabId: string, id: Id ) => void ) => JSX.Element;
     readPermission?: string;
