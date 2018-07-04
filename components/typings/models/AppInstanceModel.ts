4d4b12515d19c424ef1ee0fd4d3b2f8a85ce4bbb
Christopher Vuong <christopher.vuong@amalto.com>
2018-07-04 09:58:40 +0200

    Export AppEndpointsModel and AppInstanceModel from typings

---
+++ components/typings/models/AppInstanceModel.ts
@@ -0,0 +1,31 @@
+/**
+ * Created by Bruno Grieder on 09 April 2015.
+ */
+
+
+module AppInstanceModel {
+    export interface Properties {
+        baseContext?: string;
+        infoPath?: string;
+        communityPortalUrl?: string;
+    }
+}
+
+
+interface AppInstanceModel {
+    name?: string
+    description?: string
+    enabled?: boolean
+    orgRootId?: string;
+    properties?: AppInstanceModel.Properties
+}
+
+interface AppInstanceSearch {
+    pageSize?: number;
+    pageContext?: string;
+    searchColumn?: 'NAME' | 'DESCRIPTION';
+    searchTerm?: string;
+    orderDesc?: boolean;
+}
+
+export { AppInstanceModel, AppInstanceSearch }
