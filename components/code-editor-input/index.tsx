6eefde4c97af360d13662d88b146fe3445dfaa44
Christopher Vuong <christopher.vuong@amalto.com>
2018-09-12 16:24:20 +0200

    Use brace on code-editor-input component

--- components/code-editor-input/index.tsx
+++ components/code-editor-input/index.tsx
@@ -3,12 +3,71 @@ import * as React from 'react'
 import { WrappedFieldProps, Field, BaseFieldProps } from 'redux-form'
 import * as classNames from 'classnames'
 import * as uuid from 'uuid'
+import * as ace from 'brace'
 import { AceSession, UserModel, getUserJson } from '@amalto/typings'
 
 // Components
 import Help from '@amalto/help'
 
-type AceEditor = AceAjax.Editor
+// type AceEditor = AceAjax.Editor
+
+/**
+ * Ace editor mode
+ */
+import 'brace/mode/csharp'
+import 'brace/mode/dockerfile'
+import 'brace/mode/groovy'
+import 'brace/mode/html'
+import 'brace/mode/java'
+import 'brace/mode/javascript'
+import 'brace/mode/json'
+import 'brace/mode/typescript'
+import 'brace/mode/tsx'
+import 'brace/mode/xml'
+import 'brace/mode/sql'
+
+/**
+ * Ace editor theme
+ */
+import 'brace/theme/ambiance'
+import 'brace/theme/chaos'
+import 'brace/theme/chrome'
+import 'brace/theme/clouds'
+import 'brace/theme/clouds_midnight'
+import 'brace/theme/cobalt'
+import 'brace/theme/crimson_editor'
+import 'brace/theme/dawn'
+import 'brace/theme/dracula'
+import 'brace/theme/dreamweaver'
+import 'brace/theme/eclipse'
+import 'brace/theme/github'
+import 'brace/theme/gob'
+import 'brace/theme/gruvbox'
+import 'brace/theme/idle_fingers'
+import 'brace/theme/iplastic'
+import 'brace/theme/katzenmilch'
+import 'brace/theme/kr_theme'
+import 'brace/theme/kuroir'
+import 'brace/theme/merbivore'
+import 'brace/theme/merbivore_soft'
+import 'brace/theme/mono_industrial'
+import 'brace/theme/monokai'
+import 'brace/theme/pastel_on_dark'
+import 'brace/theme/solarized_dark'
+import 'brace/theme/solarized_light'
+import 'brace/theme/sqlserver'
+import 'brace/theme/terminal'
+import 'brace/theme/textmate'
+import 'brace/theme/tomorrow'
+import 'brace/theme/tomorrow_night'
+import 'brace/theme/tomorrow_night_blue'
+import 'brace/theme/tomorrow_night_bright'
+import 'brace/theme/tomorrow_night_eighties'
+import 'brace/theme/twilight'
+import 'brace/theme/vibrant_ink'
+import 'brace/theme/xcode'
+
+type AceEditor = ace.Editor
 
 
 /**
@@ -287,7 +346,7 @@ class CodeEditor extends React.Component<CodeEditor.Props, CodeEditor.State> {
 
     }
 
-    private setEditorOptions = ( editor: AceAjax.Editor ) => {
+    private setEditorOptions = ( editor: AceEditor ) => {
 
         const userJson = this.props.user ? getUserJson( this.props.user ) : {}
 
@@ -399,7 +458,7 @@ class CodeEditor extends React.Component<CodeEditor.Props, CodeEditor.State> {
         editor.setShowPrintMargin( false )
     }
 
-    private isSessionSavable = ( editor: AceAjax.Editor ): boolean => {
+    private isSessionSavable = ( editor: AceEditor ): boolean => {
         const annotations: AceAjax.Annotation[] = editor.getSession().getAnnotations()
         let isSavable: boolean = true
 
