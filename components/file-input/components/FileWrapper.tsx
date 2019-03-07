c746ab0b4c4933f215f6d612941b044da0955701
Christopher Vuong <christopher.vuong@amalto.com>
2019-03-07 09:53:46 +0100

    70: FileWrapper in file-input component as function component

--- components/file-input/components/FileWrapper.tsx
+++ components/file-input/components/FileWrapper.tsx
@@ -6,7 +6,7 @@ import * as classNames from 'classnames'
 import { formatFileSize } from '@amalto/helpers'
 
 module FileWrapper {
-    export interface Props extends React.ClassAttributes<FileWrapper> {
+    export interface Props {
         fileName: string;
         fileSize: number;
         deleteUploadedFile: ( fileName: string ) => void;
@@ -17,75 +17,61 @@ module FileWrapper {
         message?: string;
         successMessage?: string
     }
-
-    export interface State {
-
-    }
 }
 
-class FileWrapper extends React.Component<FileWrapper.Props, FileWrapper.State> {
+function FileWrapper( props: FileWrapper.Props ): JSX.Element {
 
-    constructor( props: FileWrapper.Props ) {
-        super( props )
-    }
-
-    render() {
-
-        let progressDisplay = this.props.progress ? (
-            <div className="file-upload-progress" style={{ width: this.props.progress + '%' }}></div>
-        ) : !this.props.uploaded ? <div className="bar-load" /> : null
-
-        let deleteBtn = ( this.props.uploaded && !this.props.processSuccess ) ? (
-            <div data-file-name={this.props.fileName} className="file-icon-cell file-delete-btn" onClick={this.deleteUploadedFile}>
-                <span className="fas fa-fw fa-trash-alt"></span>
-            </div>
-        ) : null
-
-        let successIcon = this.props.processSuccess ? (
-            <div className="file-icon-cell">
-                <span className="fas fa-fw fa-check"></span>
-            </div>
-        ) : null
-
-        let createdMessageText = this.props.successMessage || null
-
-        let errorMessage = ( this.props.processSuccess === false && this.props.message ) ? (
-            <div className="file-error-row">
-                <div className="file-error-message">
-                    <span className="fas fa-exclamation-triangle right-spaced" />
-                    <span dangerouslySetInnerHTML={{ __html: this.props.message }} />
-                </div>
-                {/* this div matches the number of cell of the previous table-row, it allows proper alignment of borders */}
-                <div>&nbsp;</div>
-            </div>
-        ) : null
-
-        return (
-            <li className={classNames( {
-                'success-color': this.props.processSuccess,
-                'warning-color': this.props.processState === 'WARNING',
-                'danger-color': this.props.processState === 'ERROR',
-                'bar-loading-wrapper': !this.props.uploaded
-            } )}>
-                {progressDisplay}
-                <div className="file-details">
-                    <span className="file-name" data-toggle="tooltip" data-original-title={createdMessageText}>{this.props.fileName}</span>
-                    <span className="file-size">{formatFileSize( this.props.fileSize )}</span>
-                </div>
-                {deleteBtn}
-                {successIcon}
-                {errorMessage}
-            </li>
-        )
-
-    }
-
-    private deleteUploadedFile = ( event: any ): void => {
+    const deleteUploadedFile = ( event: any ): void => {
         const fileName = event.currentTarget.getAttribute( 'data-file-name' )
-        this.props.deleteUploadedFile( fileName )
+        props.deleteUploadedFile( fileName )
     }
 
-}
+    let progressDisplay = props.progress ? (
+        <div className="file-upload-progress" style={{ width: props.progress + '%' }}></div>
+    ) : !props.uploaded ? <div className="bar-load" /> : null
 
+    let deleteBtn = ( props.uploaded && !props.processSuccess ) ? (
+        <div data-file-name={props.fileName} className="file-icon-cell file-delete-btn" onClick={deleteUploadedFile}>
+            <span className="fas fa-fw fa-trash-alt"></span>
+        </div>
+    ) : null
+
+    let successIcon = props.processSuccess ? (
+        <div className="file-icon-cell">
+            <span className="fas fa-fw fa-check"></span>
+        </div>
+    ) : null
+
+    let createdMessageText = props.successMessage || null
+
+    let errorMessage = ( props.processSuccess === false && props.message ) ? (
+        <div className="file-error-row">
+            <div className="file-error-message">
+                <span className="fas fa-exclamation-triangle right-spaced" />
+                <span dangerouslySetInnerHTML={{ __html: props.message }} />
+            </div>
+            {/* this div matches the number of cell of the previous table-row, it allows proper alignment of borders */}
+            <div>&nbsp;</div>
+        </div>
+    ) : null
+
+    return (
+        <li className={classNames( {
+            'success-color': props.processSuccess,
+            'warning-color': props.processState === 'WARNING',
+            'danger-color': props.processState === 'ERROR',
+            'bar-loading-wrapper': !props.uploaded
+        } )}>
+            {progressDisplay}
+            <div className="file-details">
+                <span className="file-name" data-toggle="tooltip" data-original-title={createdMessageText}>{props.fileName}</span>
+                <span className="file-size">{formatFileSize( props.fileSize )}</span>
+            </div>
+            {deleteBtn}
+            {successIcon}
+            {errorMessage}
+        </li>
+    )
+}
 
 export default FileWrapper
