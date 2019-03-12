9719abf4ee7826933d7b5f3320729d26ec85d3c0
Christopher Vuong <christopher.vuong@amalto.com>
2019-03-12 16:54:46 +0100

    70: Use hooks on ColorPicker

---
+++ components/color-picker/constants/colors.ts
@@ -0,0 +1,8 @@
+export const COLORS: string[] = [
+    "#ffffff", "#000000", "#b4dcfa", "#5eccf3", "#a7ea52", "#5dceaf", "#ff8021", "#f14124",
+    "#f0f3f3", "#7e7e7e", "#8bc9f7", "#def4fc", "#edfadc", "#def5ef", "#ffe5d2", "#fcd9d3",
+    "#d8d8d8", "#565656", "#4facf3", "#beeafa", "#dbf6b9", "#beebdf", "#ffcca6", "#f9b3a7",
+    "#bfbfbf", "#3a3a3a", "#0d78c9", "#9ee0f7", "#caf297", "#9de1cf", "#ffb279", "#f68d7b",
+    "#a5a5a5", "#262626", "#063c64", "#11b2eb", "#81d319", "#34ac8b", "#d85c00", "#c3260c",
+    "#7f7f7f", "#0c0c0c", "#021828", "#0b769c", "#568c11", "#22725c", "#903d00", "#821908"
+]
