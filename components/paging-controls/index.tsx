419826f1f0bb296223482e0ee09809e4082213d5
Christopher Vuong <christopher.vuong@amalto.com>
2019-03-18 17:32:25 +0100

    70: Use hooks on PagingControls

--- components/paging-controls/index.tsx
+++ components/paging-controls/index.tsx
@@ -1,13 +1,9 @@
 // Modules
 import * as React from 'react'
 import { FormattedNumber } from 'react-intl'
-import * as classNames from 'classnames'
 
 // Utils
-import { compileWordings } from '@amalto/helpers'
-
-// Wordings
-import { MULTILANGUAGE_WORDINGS } from '@amalto/wordings'
+import { getWordings } from '@amalto/helpers'
 
 /**
  * Page navigation toolbar.
@@ -16,7 +12,7 @@ import { MULTILANGUAGE_WORDINGS } from '@amalto/wordings'
  * which are accessible at the root component of your service.
  */
 module PagingControls {
-    export interface Props extends React.Props<PagingControls> {
+    export interface Props {
         /** CSS class wrapping the component. */
         containerClass?: string;
         /** Current page selected. */
@@ -36,152 +32,136 @@ module PagingControls {
          * Accessible via [WebStorage](#webstorage).
          */
         locale: string;
-
-        /** Hide props from documentation */
-
-        /** @ignore */
-        children?: React.ReactNode;
-        /** @ignore */
-        key?: React.ReactText;
-        /** @ignore */
-        ref?: React.Ref<PagingControls>;
-    }
-
-    export interface State {
-        wordings?: { [key: string]: string };
     }
 }
 
+function PagingControls( props: PagingControls.Props ) {
+    const [wordings, setWordings] = React.useState( {} as any )
 
-class PagingControls extends React.Component<PagingControls.Props, PagingControls.State> {
-    private goToPageInput: HTMLInputElement
+    const { containerClass, currentPage, totalPages, byContext } = props
 
-    constructor( props: PagingControls.Props ) {
-        super( props )
-        this.state = {
-            wordings: compileWordings( MULTILANGUAGE_WORDINGS, props.locale )
-        }
-        this.goToPageInput = undefined
+    const goToPageInput = React.useRef( null )
+
+    React.useEffect( () => {
+        setWordings( getWordings( {}, props.locale ) )
+    }, [props.locale] )
+
+    //basic paging control by page number        
+    let prevDisabled = currentPage === 1
+    let nextDisabled = totalPages === 1 || ( currentPage === totalPages )
+
+    //paging control by context tokens
+    if ( byContext ) {
+        prevDisabled = !byContext.prevContextToken
+        nextDisabled = !byContext.nextContextToken
     }
 
-    render() {
+    return (
 
-        const { wordings } = this.state
+        totalPages !== 1 ? (
 
-        const { containerClass, currentPage, totalPages, byContext } = this.props
+            <div className={containerClass}>
 
-        //basic paging control by page number        
-        let prevDisabled = currentPage === 1
-        let nextDisabled = totalPages === 1 || ( currentPage === totalPages )
+                {
+                    byContext ? null : (
+                        <button type="button" disabled={prevDisabled} className="btn btn-primary btn-trans" onClick={e => goToFirstPage( e, props.handlePageChange )}>
+                            <span className="fas fa-fw fa-fast-backward"></span>
+                        </button>
+                    )
+                }
 
-        //paging control by context tokens
-        if ( byContext ) {
-            prevDisabled = !byContext.prevContextToken
-            nextDisabled = !byContext.nextContextToken
-        }
+                <button type="button" disabled={prevDisabled} className="btn btn-primary btn-trans" onClick={e => goToPrevPage( e, props )}>
+                    <span className="fas fa-fw fa-backward"></span>
+                </button>
 
-        return (
+                <span className="btn btn-primary btn-trans text-element no-border-right">
+                    <span className="right-spaced">{wordings.page}</span>
+                    <strong className="right-spaced">
+                        {
+                            byContext ? <FormattedNumber value={currentPage} /> : (
+                                <input
+                                    style={{ color: '#61a653' }}
+                                    type="text"
+                                    size={( totalPages ).toString().length}
+                                    value={currentPage}
+                                    onChange={e => handleGoToPageInput( e, props )}
+                                    ref={goToPageInput}
+                                    onFocus={() => { goToPageInput.current.select() }}
+                                />
+                            )
+                        }
+                    </strong>
+                    <span className="right-spaced">{wordings.of}</span>
+                    <span><FormattedNumber value={totalPages} /></span>
+                </span>
 
-            totalPages !== 1 ? (
+                <button type="button" disabled={nextDisabled} className="btn btn-primary btn-trans" onClick={e => goToNextPage( e, props )}>
+                    <span className="fas fa-fw fa-forward"></span>
+                </button>
 
-                <div className={containerClass}>
+                {
+                    byContext ? null : (
+                        <button type="button" disabled={nextDisabled} className="btn btn-primary btn-trans" onClick={e => goToLastPage( e, props )}>
+                            <span className="fas fa-fw fa-fast-forward"></span>
+                        </button>
+                    )
+                }
+            </div>
+        ) : null
+    )
+}
 
-                    {
-                        byContext ? null : (
-                            <button type="button" disabled={prevDisabled} className="btn btn-primary btn-trans" onClick={this.goToFirstPage}>
-                                <span className="fas fa-fw fa-fast-backward"></span>
-                            </button>
-                        )
-                    }
+// Select page from input
+function handleGoToPageInput( event: React.ChangeEvent<HTMLInputElement>, props: PagingControls.Props ): void {
+    const { totalPages, handlePageChange } = props
+    if ( /^\d+$/.test( event.target.value ) && parseInt( event.target.value ) > 0 && parseInt( event.target.value ) <= totalPages ) {
+        handlePageChange( parseInt( event.target.value ) )
+    }
+    else {
+        this.goToPageInput.blur()
+    }
+}
 
-                    <button type="button" disabled={prevDisabled} className="btn btn-primary btn-trans" onClick={this.goToPrevPage}>
-                        <span className="fas fa-fw fa-backward"></span>
-                    </button>
+// Go back to first page
+function goToFirstPage( event: any, handlePageChange: ( newPage: number | string ) => void ): void {
+    $( event.currentTarget ).blur()//removes active state on the button
+    handlePageChange( 1 )
+}
 
-                    <span className="btn btn-primary btn-trans text-element no-border-right">
-                        <span className="right-spaced">{wordings.page}</span>
-                        <strong className="right-spaced">
-                            {
-                                byContext ? <FormattedNumber value={currentPage} /> : (
-                                    <input
-                                        type="text"
-                                        size={( totalPages ).toString().length}
-                                        value={currentPage}
-                                        onChange={this.handleGoToPageInput}
-                                        ref={input => this.goToPageInput = input}
-                                        onFocus={() => { this.goToPageInput.select() }}
-                                    />
-                                )
-                            }
-                        </strong>
-                        <span className="right-spaced">{wordings.of}</span>
-                        <span><FormattedNumber value={totalPages} /></span>
-                    </span>
+// Go to next page
+function goToNextPage( event: any, props: PagingControls.Props ): void {
+    $( event.currentTarget ).blur()
 
-                    <button type="button" disabled={nextDisabled} className="btn btn-primary btn-trans" onClick={this.goToNextPage}>
-                        <span className="fas fa-fw fa-forward"></span>
-                    </button>
+    const { byContext, handlePageChange } = props
 
-                    {
-                        byContext ? null : (
-                            <button type="button" disabled={nextDisabled} className="btn btn-primary btn-trans" onClick={this.goToLastPage}>
-                                <span className="fas fa-fw fa-fast-forward"></span>
-                            </button>
-                        )
-                    }
-                </div>
-            ) : null
-        )
+    if ( byContext ) {
+        handlePageChange( byContext.nextContextToken )
+    }
+    else {
+        handlePageChange( props.currentPage + 1 )
     }
 
-    private handleGoToPageInput = ( event: React.ChangeEvent<HTMLInputElement> ) => {
-        const { totalPages, handlePageChange } = this.props
-        if ( /^\d+$/.test( event.target.value ) && parseInt( event.target.value ) > 0 && parseInt( event.target.value ) <= totalPages ) {
-            handlePageChange( parseInt( event.target.value ) )
-        }
-        else {
-            this.goToPageInput.blur()
-        }
+}
+
+// Go to previous page
+function goToPrevPage( event: any, props: PagingControls.Props ): void {
+    $( event.currentTarget ).blur()
+
+    const { byContext, handlePageChange } = props
+
+    if ( byContext ) {
+        handlePageChange( byContext.prevContextToken )
+    }
+    else {
+        handlePageChange( props.currentPage - 1 )
     }
 
-    private goToFirstPage = ( event: any ): void => {
-        $( event.currentTarget ).blur()//removes active state on the button
-        this.props.handlePageChange( 1 )
-    }
+}
 
-    private goToNextPage = ( event: any ): void => {
-        $( event.currentTarget ).blur()
-
-        const { byContext, handlePageChange } = this.props
-
-        if ( byContext ) {
-            handlePageChange( byContext.nextContextToken )
-        }
-        else {
-            handlePageChange( this.props.currentPage + 1 )
-        }
-
-    }
-
-    private goToPrevPage = ( event: any ): void => {
-        $( event.currentTarget ).blur()
-
-        const { byContext, handlePageChange } = this.props
-
-        if ( byContext ) {
-            handlePageChange( byContext.prevContextToken )
-        }
-        else {
-            handlePageChange( this.props.currentPage - 1 )
-        }
-
-    }
-
-    private goToLastPage = ( event: any ): void => {
-        $( event.currentTarget ).blur()
-        this.props.handlePageChange( this.props.totalPages )
-    }
-
+// Go to last page
+function goToLastPage( event: any, props: PagingControls.Props ): void {
+    $( event.currentTarget ).blur()
+    props.handlePageChange( props.totalPages )
 }
 
 export default PagingControls
