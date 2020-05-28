217e65fb7d19a8b8b76f3ecd2c2475e859a919aa
Vincent Viaud <vincent.viaud@enelis.tech>
2020-05-28 13:31:43 +0200

    [storybook] use docs preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.(js|ts|mdx)",
-    "../src/**/*.stories.(js|ts|mdx)",
-  ],
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-a11y",
-    "@storybook/addon-docs",
     "@storybook/addon-knobs",
     "@storybook/addon-notes/register-panel",
+  ],
+  presets: ["@storybook/addon-docs/preset"],
+  stories: [
+    "../stories/**/*.stories.(js|ts|mdx)",
+    "../src/**/*.stories.(js|ts|mdx)",
   ],
 };
