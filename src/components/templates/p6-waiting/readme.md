440f7fbac810b8b47e872676357d6beee2cc3866
Jonathan Swale <jonathan.swale@amalto.com>
2020-06-08 16:29:03 +0200

    Create p6-waiting template

---
+++ src/components/templates/p6-waiting/readme.md
@@ -0,0 +1,21 @@
+# p6-waiting
+
+<!-- Auto Generated Below -->
+
+
+## Dependencies
+
+### Depends on
+
+- [p6-spinner](../../atoms/p6-spinner)
+
+### Graph
+```mermaid
+graph TD;
+  p6-waiting --> p6-spinner
+  style p6-waiting fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
---
+++ src/components/templates/p6-waiting/test/p6-waiting.spec.tsx
@@ -0,0 +1,20 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { P6Waiting } from "../p6-waiting";
+
+describe("p6-waiting", () => {
+  it("renders", async () => {
+    const page = await newSpecPage({
+      components: [P6Waiting],
+      html: `<p6-waiting></p6-waiting>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("with message", async () => {
+    const page = await newSpecPage({
+      components: [P6Waiting],
+      html: `<p6-waiting>Loading in progress</p6-waiting>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+});
