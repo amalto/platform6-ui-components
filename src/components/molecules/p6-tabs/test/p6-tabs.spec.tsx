a316492b792625815a91a83bffb22fbdd46eed72
Vuong Christopher <christopher.vuong@amalto.com>
2020-06-10 10:22:03 +0200

    Use snapshot on p6-tabs unit test

--- src/components/molecules/p6-tabs/test/p6-tabs.spec.tsx
+++ src/components/molecules/p6-tabs/test/p6-tabs.spec.tsx
@@ -16,11 +16,11 @@ describe("p6-tabs", () => {
     `);
   });
 
-  it("Should display the first tab", async () => {
+  it("Should display the third tab", async () => {
     const page = await newSpecPage({
       components: [P6Tabs],
       html: `
-      <p6-tabs default="first">
+      <p6-tabs default="third">
         <div id="first" title="What is Lorem Ipsum?">1</div>
         <div id="second" title="Where does it come from?">2</div>
         <div id="third" title="Why do we use it?">3</div>
@@ -29,40 +29,6 @@ describe("p6-tabs", () => {
       `,
     });
 
-    expect(page.root).toEqualHtml(`
-      <p6-tabs default="first">
-        <mock:shadow-root>
-          <div class="tabs">
-            <ul>
-              <li class="is-active">
-                <a href="#first" id="first-tab">
-                  What is Lorem Ipsum?
-                </a>
-              </li>
-              <li>
-                <a href="#second" id="second-tab">
-                  Where does it come from?
-                </a>
-              </li>
-              <li>
-                <a href="#third" id="third-tab">
-                  Why do we use it?
-                </a>
-              </li>
-              <li>
-                <a href="#fourth" id="fourth-tab">
-                  Where can I get some?
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="tab-content">1</div>
-        </mock:shadow-root>
-        <div id="first" title="What is Lorem Ipsum?">1</div>
-        <div id="second" title="Where does it come from?">2</div>
-        <div id="third" title="Why do we use it?">3</div>
-        <div id="fourth" title="Where can I get some?">4</div>
-      </p6-tabs>
-    `);
+    expect(page.root).toMatchSnapshot();
   });
 });
