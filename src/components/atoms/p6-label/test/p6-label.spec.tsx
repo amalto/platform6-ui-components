f338cc6ae21e8c730fbd089cf0d80627e19f813a
Vincent Viaud <vincent.viaud@enelis.tech>
2020-06-16 11:34:01 +0200

    add label component

---
+++ src/components/atoms/p6-label/test/p6-label.spec.tsx
@@ -0,0 +1,12 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { P6Label } from "../p6-label";
+
+describe("p6-label", () => {
+  it("renders", async () => {
+    const page = await newSpecPage({
+      components: [P6Label],
+      html: `<p6-label></p6-label>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+});
