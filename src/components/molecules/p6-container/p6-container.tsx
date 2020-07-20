f43095e6457d78c9660fd4b75239949f14d75a91
Vuong Christopher <christopher.amalto@gmail.com>
2020-07-20 08:47:48 +0200

    p6-container WIP init

---
+++ src/components/molecules/p6-container/p6-container.tsx
@@ -0,0 +1,16 @@
+import { Component, h, Host } from "@stencil/core";
+
+@Component({
+  tag: "p6-container",
+  styleUrl: "p6-container.scss",
+  shadow: true,
+})
+export class P6Container {
+  render(): HTMLP6ContainerElement {
+    return (
+      <Host>
+        <slot />
+      </Host>
+    );
+  }
+}
---
+++ src/components/molecules/p6-container/test/p6-container.spec.tsx
@@ -0,0 +1,18 @@
+import { newSpecPage } from "@stencil/core/testing";
+import { P6Container } from "../p6-container";
+
+describe("p6-container", () => {
+  it("renders", async () => {
+    const page = await newSpecPage({
+      components: [P6Container],
+      html: `<p6-container></p6-container>`,
+    });
+    expect(page.root).toEqualHtml(`
+      <p6-container>
+        <mock:shadow-root>
+          <slot></slot>
+        </mock:shadow-root>
+      </p6-container>
+    `);
+  });
+});
