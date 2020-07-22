0d6b62469f2ef255d58281a5f44013de4be49778
Jonathan Swale <jonathan.swale@amalto.com>
2020-07-22 10:31:37 +0200

    Add viewport plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,15 @@
 module.exports = {
   addons: [
+    // https://github.com/storybookjs/storybook/tree/master/addons/actions
     "@storybook/addon-actions",
+    // https://github.com/storybookjs/storybook/tree/master/addons/a11y
     "@storybook/addon-a11y",
+    // https://github.com/storybookjs/storybook/tree/master/addons/docs
     "@storybook/addon-docs",
+    // https://github.com/storybookjs/storybook/tree/next/addons/controls
     "@storybook/addon-controls",
+    // https://github.com/storybookjs/storybook/tree/master/addons/viewport
+    "@storybook/addon-viewport/register",
   ],
   stories: [
     "../stories/**/*.stories.@(ts|mdx)",
