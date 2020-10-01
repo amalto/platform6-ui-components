78806b9bed0704ee69b626ece557f1134ea6635a
Vuong Christopher <christopher.amalto@gmail.com>
2020-10-01 10:04:27 +0200

    Update FileWrapper interface

--- components/typings/models/FileWrapper.ts
+++ components/typings/models/FileWrapper.ts
@@ -1,16 +1,13 @@
-/**
- * Created by franckmontaigne on 22/01/16.
- */
-
 interface FileWrapper {
-    sourceFile: File;
-    uploadStarted: boolean;
-    uploadProgress: number;
-    uploadEnded: boolean;
-    processSuccess?: boolean;
-    processState?: string;
-    message?: string;
-    createdMessagesIds?: string;
+  sourceFile: File;
+  uploadStarted: boolean;
+  uploadProgress: number;
+  uploadEnded: boolean;
+  processSuccess?: boolean;
+  processState?: string;
+  message?: string;
+  createdMessagesIds?: string;
+  createdTransactionsIds?: string;
 }
 
 export default FileWrapper
