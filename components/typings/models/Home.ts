63e55cee83dbf68fa5eb5d447fe4d415953f556e
Vuong Christopher <christopher.vuong@amalto.com>
2021-09-16 15:30:40 +0200

    Replace OrderDataset by ConfigurationItem

--- components/typings/models/Home.ts
+++ components/typings/models/Home.ts
@@ -1,12 +1,20 @@
 /**
- * Created by Christopher VUONG on 03/03/2017 (DD:MM:YYYY)
+ * Created by Christopher VUONG on 03/03/2017
  */
 
-export interface OrderDataset {
-    id: string;
-    order: number;
-    type: string;
+/**
+ * @deprecated Use ConfigurationItem instead
+ */
+export interface OrderDataset extends ConfigurationItem { }
+
+export interface ConfigurationItem {
+    id: string; // The id of the module.
+    order: number; // The order of the module.
+    type: string; // The type of the module. 'counters' | 'reports' | 'frames'
     width: string;
     height: string;
-    data?: any;
+    data: Record<string, unknown>;
+    tmp?: boolean;
 }
+
+export declare type ConfigurationItems = ConfigurationItem[]
